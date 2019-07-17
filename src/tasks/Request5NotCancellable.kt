a936b4d60167c3375872b4f48c2e1ab84f7abfda
Lothar Schulz <lothar.schulz@moovel.com>
2019-07-17 17:15:50 +0200

    5 not cancellable

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,21 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    // this scope inherits the context from the outer scope
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
+        GlobalScope.async {// started in global scope
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { log("repo: ${repo.name} contained $it contributors") }
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    return deferreds.awaitAll().flatten().aggregate()
+
 }
