2cde55c2cb9b4eb9fa6b3cbe8f4dd284512ebb54
Lothar Schulz <lothar.schulz@moovel.com>
2019-07-17 12:46:12 +0200

    code comments from playbook

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,13 +4,14 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
+    // this scope inherits the context from the outer scope
     val repos = service
         .getOrgRepos(req.org)
         .also { logRepos(req, it) }
         .body() ?: listOf()
 
     val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
-        async(Dispatchers.Default) {
+        async(Dispatchers.Default) {// nested coroutine started with the inherited context
             service
                 .getRepoContributors(req.org, repo.name)
                 .also { log("repo: ${repo.name} contained $it contributors") }
