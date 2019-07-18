6725dec8b39a075a8fe1e218e24ed22319942f04
Lothar Schulz <lothar.sch@gmx.de>
2019-07-18 15:22:15 +0200

    channel coroutineScope fix

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,41 +1,33 @@
 package tasks
 
-import contributors.GitHubService
-import contributors.RequestData
-import contributors.User
-import contributors.logRepos
+import contributors.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
-import samples.log
+import kotlinx.coroutines.launch
 
 suspend fun loadContributorsChannels(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
-) {
-    coroutineScope {
-        val repos = service
-            .getOrgRepos(req.org)
-            .also { logRepos(req, it) }
-            .bodyList()
+) = coroutineScope {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
 
-        var allUsers = emptyList<User>()
-        val channel = Channel<List<User>>()
-
-        for ((index, repo) in repos.withIndex()) {
-            log("for start")
+    val channel = Channel<List<User>>()
+    var allUsers = emptyList<User>()
+    for (repo in repos) {
+        launch {
             val users = service.getRepoContributors(req.org, repo.name)
-                .also { log("repo: ${repo.name}") }
+                .also { logUsers(repo, it) }
                 .bodyList()
-            log("users: ${users}")
             channel.send(users)
-            log("after channel.send(users)")
         }
-        repeat(repos.size) {
-            val users = channel.receive()
-            allUsers = (allUsers + users).aggregate()
-            updateResults(allUsers, it == repos.lastIndex)
-        }
-
+    }
+    repeat(repos.size) {
+        val users = channel.receive()
+        allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, it == repos.lastIndex)
     }
 }
